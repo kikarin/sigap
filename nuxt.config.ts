b7257173080f7da0e98b9369da2efd3389a6d7a9
kikarin <akunamazon4811@gmail.com>
2025-12-28 14:10:09 +0700

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,48 @@
+import tailwindcss from '@tailwindcss/vite'
+import { definePreset } from '@primeuix/themes'
+import Aura from '@primeuix/themes/aura'
+import { fileURLToPath } from 'node:url'
+
+const MyCustomTheme = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '#eef2ff',
+      100: '#e0e7ff',
+      200: '#c7d2fe',
+      300: '#a5b4fc',
+      400: '#818cf8',
+      500: '#2F52BD', 
+      600: '#1e3a8a', 
+      700: '#1e40af',
+      800: '#1e3a8a',
+      900: '#172554',
+      950: '#0f172a'
+    }
+  }
+})
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  css: [fileURLToPath(new URL('./assets/css/main.css', import.meta.url))],
+
+  modules: ['@primevue/nuxt-module'],
+
+  primevue: {
+    options: {
+      ripple: true,
+      theme: {
+        preset: MyCustomTheme, 
+        options: {
+          darkModeSelector: false, 
+          cssLayer: false 
+        }
+      }
+    }
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+})
