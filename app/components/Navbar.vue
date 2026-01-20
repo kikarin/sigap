b430015fdba2891ef59ed1228de9a861c7e2096f
kikarin <akunamazon4811@gmail.com>
2026-01-20 17:39:44 +0700

    update

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -8,17 +8,23 @@
       <NuxtLink
         v-if="value.type === 'danger'"
         :to="value.link"
-        class="w-16 h-16 bg-rose-600 border border-white text-white rounded-full flex items-center justify-center shadow-lg -mt-16"
+        class="w-16 h-16 bg-rose-600 border-4 border-white text-white rounded-full flex items-center justify-center shadow-lg -mt-16 transition-all duration-300 hover:scale-110 active:scale-95"
       >
         <p class="text-[10px] font-bold">{{ value.name }}</p>
       </NuxtLink>
       <NuxtLink
         v-else 
         :to="value.link"
-        class="flex flex-col items-center gap-1"
+        class="flex flex-col items-center gap-1 transition-all duration-300 hover:scale-110 active:scale-95"
       >
-        <i :class="['pi text-xl duration-300 ease-in-out', value.icon, isActive(value.link) ? 'text-blue-500' : 'text-gray-500']"></i>
-        <p class="text-sm duration-300 ease-in-out " :class="isActive(value.link) ? 'text-blue-500' : 'text-gray-500'">{{ value.name }}</p>
+        <div class="relative">
+          <i :class="['pi text-xl transition-all duration-300', value.icon, isActive(value.link) ? 'text-blue-500 scale-110' : 'text-gray-500']"></i>
+          <div 
+            v-if="isActive(value.link)"
+            class="absolute -bottom-1 left-1/2 -translate-x-1/2 w-1 h-1 bg-blue-500 rounded-full animate-pulse"
+          ></div>
+        </div>
+        <p class="text-xs transition-all duration-300 font-medium" :class="isActive(value.link) ? 'text-blue-500' : 'text-gray-500'">{{ value.name }}</p>
       </NuxtLink>
     </div>
   </div>
